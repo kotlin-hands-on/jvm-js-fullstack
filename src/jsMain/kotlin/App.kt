5e19801af68969e02c15d61db4b2907090b13fa4
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-04-16 17:23:54 +0200

    Add solution including MongoDB

---
+++ src/jsMain/kotlin/App.kt
@@ -0,0 +1,47 @@
+import react.*
+import react.dom.*
+import kotlinext.js.*
+import kotlinx.html.js.*
+import kotlinx.coroutines.*
+
+val scope = MainScope()
+
+val App = functionalComponent<RProps> { _ ->
+    val (shoppingList, setShoppingList) = useState(emptyList<ShoppingListItem>())
+
+    useEffect(dependencies = listOf()) {
+        scope.launch {
+            setShoppingList(getShoppingList())
+        }
+    }
+
+    h1 {
+        +"Full-Stack Shopping List"
+    }
+    ul {
+        shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
+            li {
+                key = item.toString()
+                attrs.onClickFunction = {
+                    scope.launch {
+                        deleteShoppingListItem(item)
+                        setShoppingList(getShoppingList())
+                    }
+                }
+                +"[${item.priority}] ${item.desc} "
+            }
+        }
+    }
+    child(
+        functionalComponent = InputComponent,
+        props = jsObject {
+            onSubmit = { input ->
+                val cartItem = ShoppingListItem(input.replace("!", ""), input.count { it == '!' })
+                scope.launch {
+                    addShoppingListItem(cartItem)
+                    setShoppingList(getShoppingList())
+                }
+            }
+        }
+    )
+}
