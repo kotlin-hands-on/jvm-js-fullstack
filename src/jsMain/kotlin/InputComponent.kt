c1e5deabdb84af77ec600fcc3d53e19e964528aa
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-04-16 17:23:54 +0200

    Add solution including MongoDB

---
+++ src/jsMain/kotlin/InputComponent.kt
@@ -0,0 +1,33 @@
+import react.*
+import react.dom.*
+import kotlinx.html.js.*
+import kotlinx.html.InputType
+import org.w3c.dom.events.Event
+import org.w3c.dom.HTMLInputElement
+
+external interface InputProps : RProps {
+    var onSubmit: (String) -> Unit
+}
+
+val InputComponent = functionalComponent<InputProps> { props ->
+    val (text, setText) = useState("")
+
+    val submitHandler: (Event) -> Unit = {
+        it.preventDefault()
+        setText("")
+        props.onSubmit(text)
+    }
+
+    val changeHandler: (Event) -> Unit = {
+        val value = (it.target as HTMLInputElement).value
+        setText(value)
+    }
+
+    form {
+        attrs.onSubmitFunction = submitHandler
+        input(InputType.text) {
+            attrs.onChangeFunction = changeHandler
+            attrs.value = text
+        }
+    }
+}
--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,3 +1,65 @@
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.litote.kmongo.*
+import org.litote.kmongo.async.*
+import org.litote.kmongo.coroutine.*
+import org.litote.kmongo.async.getCollection
+import com.mongodb.ConnectionString
+
+val connectionString: ConnectionString? = System.getenv("MONGODB_URI")?.let {
+    ConnectionString("$it?retryWrites=false")
+}
+
+val client = if (connectionString != null) KMongo.createClient(connectionString) else KMongo.createClient()
+val database = client.getDatabase(connectionString?.database ?: "test")
+val collection = database.getCollection<ShoppingListItem>()
+
 fun main() {
-    println("Hello, JVM!")
+    val port = System.getenv("PORT")?.toInt() ?: 9090
+    embeddedServer(Netty, port) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
+
+        routing {
+            get("/") {
+                call.respondText(
+                    this::class.java.classLoader.getResource("index.html")!!.readText(),
+                    ContentType.Text.Html
+                )
+            }
+            static("/") {
+                resources("")
+            }
+            get(ShoppingListItem.path) {
+                call.respond(collection.find().toList())
+            }
+            post(ShoppingListItem.path) {
+                collection.insertOne(call.receive<ShoppingListItem>())
+                call.respond(HttpStatusCode.OK)
+            }
+            delete(ShoppingListItem.path + "/{id}") {
+                val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
+                collection.deleteOne(ShoppingListItem::id eq id)
+                call.respond(HttpStatusCode.OK)
+            }
+        }
+    }.start(wait = true)
 }
