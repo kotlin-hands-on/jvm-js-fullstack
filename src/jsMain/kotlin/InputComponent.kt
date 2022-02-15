b7f7c137711b7601d267b7d32816f83bc67f6ad7
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2022-02-15 16:36:22 +0100

    Update to new Kotlin-React DSL

--- src/jsMain/kotlin/InputComponent.kt
+++ src/jsMain/kotlin/InputComponent.kt
@@ -1,33 +1,35 @@
+import org.w3c.dom.HTMLFormElement
 import react.*
-import react.dom.*
-import kotlinx.html.js.*
-import kotlinx.html.InputType
-import org.w3c.dom.events.Event
 import org.w3c.dom.HTMLInputElement
+import react.dom.events.ChangeEventHandler
+import react.dom.events.FormEventHandler
+import react.dom.html.InputType
+import react.dom.html.ReactHTML.form
+import react.dom.html.ReactHTML.input
 
 external interface InputProps : Props {
     var onSubmit: (String) -> Unit
 }
 
-val inputComponent = fc<InputProps> { props ->
+val InputComponent = FC<InputProps> { props ->
     val (text, setText) = useState("")
 
-    val submitHandler: (Event) -> Unit = {
+    val submitHandler: FormEventHandler<HTMLFormElement> = {
         it.preventDefault()
         setText("")
         props.onSubmit(text)
     }
 
-    val changeHandler: (Event) -> Unit = {
-        val value = (it.target as HTMLInputElement).value
-        setText(value)
+    val changeHandler: ChangeEventHandler<HTMLInputElement> = {
+        setText(it.target.value)
     }
 
     form {
-        attrs.onSubmitFunction = submitHandler
-        input(InputType.text) {
-            attrs.onChangeFunction = changeHandler
-            attrs.value = text
+        onSubmit = submitHandler
+        input {
+            type = InputType.text
+            onChange = changeHandler
+            value = text
         }
     }
 }
