6fcd4c13c2cbbc82b0991bc0d2135b6420e1a623
CristianGM <cristiangm@gmail.com>
2022-05-02 15:41:01 +0200

    Gradle: use Configuration avoidance
    More info: https://docs.gradle.org/current/userguide/task_configuration_avoidance.html
    
    Scan before the change: https://scans.gradle.com/s/zrctwrngu2nma/performance/configuration#summary-task-totals
    
    Scan after the change: https://scans.gradle.com/s/obeyauaon7wq6/performance/configuration#summary-task-totals
    
    (cherry picked from commit ddeb737a66dee6b66b0f5feb42c724deb91cfd9f)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     repositories {
         mavenCentral()
-        maven { setUrl("https://plugins.gradle.org/m2/") }
+        gradlePluginPortal()
     }
 }
 rootProject.name = "shoppinglist"
