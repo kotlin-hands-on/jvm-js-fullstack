410b4ee15e6df9785a04d0571f4fbd7772d5fc7a
Anton Galitch <galitch@google.com>
2022-06-25 20:42:18 +0200

    Update the versions of the dependencies. This fixes a build issue with Apple silicone and nodeJS.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
-val kotlinVersion = "1.6.10"
+val kotlinVersion = "1.7.0"
 val serializationVersion = "1.3.2"
-val ktorVersion = "1.6.7"
+val ktorVersion = "2.0.2"
 val logbackVersion = "1.2.10"
 val reactVersion = "17.0.2-pre.299-kotlin-1.6.10"
 val kmongoVersion = "4.5.0"
 
 plugins {
-    kotlin("multiplatform") version "1.6.10"
+    kotlin("multiplatform") version "1.7.0"
     application //to run JVM part
-    kotlin("plugin.serialization") version "1.6.10"
+    kotlin("plugin.serialization") version "1.7.0"
 }
 
 group = "org.example"
