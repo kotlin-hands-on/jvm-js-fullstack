660023944363b7bc02f07715cdd34448e6deeda0
Danil Pavlov <danil.pavlov@jetbrains.com>
2023-06-17 12:07:27 +0200

    update: js ir and version upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
-val kotlinVersion = "1.7.20-Beta"
-val serializationVersion = "1.3.3"
-val ktorVersion = "2.0.3"
+val kotlinVersion = "1.8.21"
+val serializationVersion = "1.5.1"
+val ktorVersion = "2.3.0"
 val logbackVersion = "1.2.11"
-val kotlinWrappersVersion = "1.0.0-pre.354"
+val kotlinWrappersVersion = "1.0.0-pre.561"
 val kmongoVersion = "4.5.0"
 
 plugins {
-    kotlin("multiplatform") version "1.7.20-Beta"
+    kotlin("multiplatform") version "1.8.21"
     application //to run JVM part
-    kotlin("plugin.serialization") version "1.7.20-Beta"
+    kotlin("plugin.serialization") version "1.8.21"
 }
 
 group = "org.example"
